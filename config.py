c661026e7b6a398d58a36014bee280fa23c34044
abderrahimJK <abderrahim.aitbouna@gmail.com>
2023-08-21 21:18:12 +0100

    Configurations

---
+++ config.py
@@ -0,0 +1,15 @@
+config = {
+    "google_api_key": "AIzaSyDACo0HZwKENHxcCdZK5HCz0lJ3_3-u56g",
+    "youtube_playlist_id": "PLqhdsGA2mWTdiuJxygOHdWAeJPIpofO4Q",
+    "kafka": {
+        "bootstrap.servers": "pkc-lzvrd.us-west4.gcp.confluent.cloud:9092",
+        "security.protocol": "sasl_ssl",
+        "sasl.mechanism": "PLAIN",
+        "sasl.username": "3GTOWQUZ3LKGSRYE",
+        "sasl.password": "GSbw9wG6M1sKuKwJx+EF/PvtkIk6b5ewO3kM6KTV7epNJ9+e85bxu/WcXGGxb/1B",
+    },
+    "schema_registry": {
+        "url": "https://psrc-o268o.eu-central-1.aws.confluent.cloud",
+        "basic.auth.user.info": "EI4JPXABTDMX63DO:LwJcpmlWfaM228j0tOCA/MKSS1Tw5vs5j/qEVkzmjYZg/NLc1rB4UYpYBFWoiggG",
+    }
+}
